df57e14c266b4b93001c6c9443be45fc5922a3d2
Dhruv <dev.aroradhruv@gmail.com>
2021-12-02 08:21:24 +0530

    Created & Shuffled cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react'
-import "./App.css"
+import { useState, React } from "react";
+import "./App.css";
+
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
 
 export default function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
-    <div className = "App">
+    <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
-  )
+  );
 }
